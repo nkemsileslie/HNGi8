054f89912fc00522f94b54ea67df0930f653db41
Nkemsinachi Leslie Onyekwuluje <62054399+nkemsileslie@users.noreply.github.com>
2021-08-17 11:51:52 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# This is a program that prints my name in python
+
+def print_name(name):
+    print(f'Hello, {name}')
+
+
+if __name__ == '__main__':
+    print_name('My Name is Onyekwuluje Nkemsinachi Leslie')
